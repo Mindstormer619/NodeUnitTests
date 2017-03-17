86de2296c7cbeec5ed14d2792ad6a8058106c94f
Siddarth R Iyer <mindstormer619@gmail.com>
2017-03-18 01:22:04 +0530

    More tests! This time featuring events!

--- services/fileReader.js
+++ services/fileReader.js
@@ -6,4 +6,27 @@ module.exports.reportFileSize = function(filename) {
     .then(function(stats) {
         return stats.size;
     });
-}
+};
+
+module.exports.getCapitalLetters = function(filename) {
+    return new Promise(function(resolve, reject) {
+        var readStream = fs.createReadStream(filename);
+
+        var caps = "";
+
+        readStream.on('data', function(chunk) {
+            var matches = chunk.toString().match(/[A-Z]+/g);
+            for (var i = 0; i < matches.length; i++) {
+                caps += matches[i];
+            }
+        });
+
+        readStream.on('end', function() {
+            resolve(caps);
+        });
+
+        readStream.on('error', function(err) {
+            reject(err);
+        });
+    });
+};
--- test/exampleTests.js
+++ test/exampleTests.js
@@ -17,14 +17,28 @@ describe('services/fileReader', function() {
     describe('#reportFileSize()', function() {
         it('should get filesize as 5000 when reading 5KB file', function() {
             return fileReader.reportFileSize('public/file5KB.txt')
-            .should.eventually.
-            equal(5000)
+            .should.eventually
+            .equal(5000)
             .and.be.above(3000)
             .and.be.below(6000);
         });
 
         it('should fail when file is not found', function() {
             return fileReader.reportFileSize('fileThatDoesntExist.txt')
+            .should.be.rejected();
+        });
+    });
+
+
+    describe('#getCapitalLetters()', function() {
+        it('should obtain nothing other than caps (A-Z) from the file', function() {
+            return fileReader.getCapitalLetters('public/file5KB.txt')
+            .should.eventually
+            .match(/^[A-Z]+/);
+        });
+
+        it('should throw exception when file does not exist', function() {
+            return fileReader.getCapitalLetters('fileThatDoesntExist.txt')
             .should.be.rejected();
         });
     });
